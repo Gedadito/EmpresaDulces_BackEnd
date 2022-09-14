2a9922c7ff1694b30597cff331a5a0366518442f
maest <maest@LAPTOP-G1Q8LEDN>
2022-09-14 15:22:19 -0500

    Agregar archivos de proyecto.

---
+++ EmpresaDulces/Controllers/TiposDeDulcesController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmpresaDulces.Controllers
+{
+    public class TiposDeDulcesController : Controller
+    {
+        // GET: TiposDeDulcesController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: TiposDeDulcesController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: TiposDeDulcesController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TiposDeDulcesController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TiposDeDulcesController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: TiposDeDulcesController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TiposDeDulcesController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: TiposDeDulcesController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
