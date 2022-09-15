03aeda897b25c40bff7fa6a049038570a123d7c4
Gedadito <gmr.110802@gmail.com>
2022-09-15 10:30:43 -0500

    Se agregan los metodos GET, POST, PUT, DELETE en el controlador TiposDeDulcesController

--- EmpresaDulces/Controllers/TiposDeDulcesController.cs
+++ EmpresaDulces/Controllers/TiposDeDulcesController.cs
@@ -2,6 +2,7 @@
 
 using EmpresaDulces.Entidades;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace EmpresaDulces.Controllers
 {
@@ -9,16 +10,66 @@ namespace EmpresaDulces.Controllers
     [Route("EmpresaDulces")]
     public class TiposDeDulcesController : ControllerBase
     {
+        private readonly AplicationDbContext dbContext;
+
+        public TiposDeDulcesController(AplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+
         [HttpGet]
         
-        public ActionResult<List<Dulces>> Get()
+        public async Task<ActionResult<List<Dulces>>> Get()
         {
-            return new List<Dulces>()
-            {
-                new Dulces() { Id = 1, NombreDelDulce = "Lucas Muecas"},
-                new Dulces() { Id = 2, NombreDelDulce = "TutsiPop" }
-            };
+
+            return await dbContext.TipoDulce.ToListAsync();
+
         }
+
+        [HttpPost]
+
+        public async Task<ActionResult> Post(Dulces dulce)
+        {
+            dbContext.Add(dulce);
+            await dbContext.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpPut("{id:int}")]
+
+        public async Task<ActionResult> Put(Dulces dulce, int id)
+        {
+            if (dulce.Id != id)
+            {
+                return BadRequest("El id del dulce no coincide");
+            }
+
+            dbContext.Update(dulce);
+            await dbContext.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpDelete("{id:int}")]
+
+        public async Task<ActionResult> Delete(int id)
+        {
+            var exist = await dbContext.TipoDulce.AnyAsync(n => n.Id == id);
+            if (!exist)
+            {
+                return NotFound();
+            }
+
+            dbContext.Remove(new Dulces()
+            {
+                Id = id
+            });
+            await dbContext.SaveChangesAsync();
+            return Ok();
+
+        }
+
+
         
     }
 }
