5114f2e7714426284e6285289b64713023c4dfc1
Gedadito <gmr.110802@gmail.com>
2022-10-24 13:49:57 -0500

    Modificamos el Startup probando las diferentes maneras de utilizar un Middleware. Creamos la carpeta Middlewares para posteriormente crear la clase a la cual llamaremos en stratup y asi utilizarla primeramente exponiendo la clase y por ultimo llamando la funcion de extension de la clase.

--- EmpresaDulces/Startup.cs
+++ EmpresaDulces/Startup.cs
@@ -2,6 +2,7 @@
 using System.Text.Json.Serialization;
 using EmpresaDulces.Services;
 using Microsoft.OpenApi.Models;
+using EmpresaDulces.Middlewares;
 
 namespace EmpresaDulces
 {
@@ -41,12 +42,59 @@ namespace EmpresaDulces
             services.AddEndpointsApiExplorer();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIAlumnos", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "EmpresaDulces", Version = "v1" });
             });
         }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
         {
+            //Use me permite agregar mi propio proceso sin afectar a los demas como Run
+            //app.Use(async (context, siguiente) =>
+            //{
+            //    using (var ms = new MemoryStream())
+            //    {
+            //        //Se asigna el body del response en una variable y se le da el valor de memorystream
+            //        var bodyOriginal = context.Response.Body;
+            //        context.Response.Body = ms;
+
+            //        //Permite continuar con la linea
+            //        await siguiente.Invoke();
+
+            //        //Guardamos lo que le respondemos al cliente en el string
+            //        ms.Seek(0, SeekOrigin.Begin);
+            //        string response = new StreamReader(ms).ReadToEnd();
+            //        ms.Seek(0, SeekOrigin.Begin);
+
+            //        //Leemos el stream y lo colocamos como estaba
+            //        await ms.CopyToAsync(bodyOriginal);
+            //        context.Response.Body = bodyOriginal;
+
+            //        logger.LogInformation(response);
+            //    }
+            //});
+
+            //Metodo para utilizar la clase middleware propia
+            //app.UseMiddleware<ResponseHttpMiddleware>();
+
+            //Metodo para utilizar la clase middleware sin exponer la clase. 
+            app.UseResponseHttpMiddleware();
+
+
+            //Atrapara todas las peticiones http que mandemos y retornar un string
+            //Para detener todos los otros middleware se utiliza la funcion RUN
+
+            //Para condicionar la ejecucion del middleware segun una ruta especifica se utiliza Map
+            //Al utilizar Map permite que en lugar de ejecutar linealmente podemos agregar rutas especificas para
+            // nuestro middleware
+            app.Map("/maping", app =>
+            {
+                app.Run(async context =>
+                {
+                    await context.Response.WriteAsync("Interceptar las peticiones");
+                });
+            });
+
+
             // Configure the HTTP request pipeline.
             if (env.IsDevelopment())
             {
